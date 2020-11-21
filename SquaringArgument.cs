59379469b8b907555b40881d54ddcd827304cd38
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2020-11-21 13:06:30 -0500

    Create SquaringArgument.cs
    CodeWars challenge to take an double type argument and square it as a double.

---
+++ SquaringArgument.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+public class Kata
+{
+  // Write a static function "Square" here which takes a double
+  // and returns the square of that double
+  static public double Square(double number)
+    {
+    double result = (double)Math.Pow(number, 2);
+    return result;
+  }
+}
