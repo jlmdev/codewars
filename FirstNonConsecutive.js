cadd5c9f778386698514d0d3e5b025bf4545c4d3
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2020-12-03 16:27:40 -0500

    Update FirstNonConsecutive.js
    I was missing the "-1" on the length to terminate the loop.

--- FirstNonConsecutive.js
+++ FirstNonConsecutive.js
@@ -1,6 +1,6 @@
 function firstNonConsecutive (arr) {
   number = arr[0];
-  for(index = 1; index <= arr.length; index++) {
+  for(index = 1; index <= arr.length - 1; index++) {
     if (arr[index] == arr[index - 1] + 1) {
       continue;
     } else {
